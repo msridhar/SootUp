388d70169cad716e01f36b197a3d4fc86a6da457
Bryce7832 <bryce.hojo@gmail.com>
2024-10-04 12:05:26 -0700

    Removed supressed warning

--- sootup.core/src/main/java/sootup/core/graph/StmtGraph.java
+++ sootup.core/src/main/java/sootup/core/graph/StmtGraph.java
@@ -631,7 +631,6 @@ public abstract class StmtGraph<V extends BasicBlock<V>> implements Iterable<Stm
     }
 
     @Override
-    @SuppressWarnings("all") // Suppressed warning flagged from Resource Leak Checker. Currently being investigated.
     public boolean hasNext() {
       final boolean hasIteratorMoreElements;
       BasicBlock<?> b = retrieveNextBlock();
@@ -653,7 +652,7 @@ public abstract class StmtGraph<V extends BasicBlock<V>> implements Iterable<Stm
           String info =
               blocks.stream()
                   .filter(n -> !iteratedBlocks.contains(n))
-                   //.map(BasicBlock::getStmts)
+                  .map(BasicBlock::getStmts)
                   .collect(Collectors.toList())
                   .toString();
           throw new IllegalStateException(
